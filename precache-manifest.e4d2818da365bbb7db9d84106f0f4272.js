b1e1d89de132c1736cbaa33a7bc44f69846915e3
muhammedalici <muhammedalici182@gmail.com>
2020-07-23 02:38:47 +0300

    Updates

---
+++ precache-manifest.e4d2818da365bbb7db9d84106f0f4272.js
@@ -0,0 +1,124 @@
+self.__precacheManifest = [
+  {
+    "revision": "0fbb7f5b1b1658a743d0",
+    "url": "/css/main.css"
+  },
+  {
+    "url": "/js/main.0fbb7f5b1b1658a743d0.js"
+  },
+  {
+    "revision": "deb5367e4b2819386744",
+    "url": "/css/vendor.7bae29f57c4ecd1fda47_1.css"
+  },
+  {
+    "url": "/js/1.deb5367e4b2819386744.js"
+  },
+  {
+    "url": "/js/2.063c0ff7a727796b7ae1.js"
+  },
+  {
+    "url": "/js/3.4caf588b516e7e8fe105.js"
+  },
+  {
+    "url": "/js/4.cd26ae45032c20673d04.js"
+  },
+  {
+    "url": "/images/logo_horizontal.e6be02cf90ab89077c8b084210629530.png"
+  },
+  {
+    "url": "/images/defaultAvatar.5dfb1249302d6d9432120cb6480352dd.jpg"
+  },
+  {
+    "url": "/images/defaultBanner.d7cf784d525a9ec138f1e4708c76a3d2.jpg"
+  },
+  {
+    "url": "/images/logo_vertical_cropped.bce502495994d82a80215c1b0d26ee7b.png"
+  },
+  {
+    "url": "/images/logo_vertical.5cffd19d1451c9a419000f65e51755b2.png"
+  },
+  {
+    "revision": "4727f0426e0db8b856aa6a13c1d8cd8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19ac989c9d4bf623d8d4fbf26443122a",
+    "url": "/404.html"
+  },
+  {
+    "revision": "e6be02cf90ab89077c8b084210629530",
+    "url": "/static/logo_horizontal.png"
+  },
+  {
+    "revision": "bce502495994d82a80215c1b0d26ee7b",
+    "url": "/static/logo_vertical_cropped.png"
+  },
+  {
+    "revision": "5cffd19d1451c9a419000f65e51755b2",
+    "url": "/static/logo_vertical.png"
+  },
+  {
+    "revision": "040a0af3019e6f44dff58c013c90c220",
+    "url": "/static/favicon.png"
+  },
+  {
+    "revision": "9e041348b72afb7287a8074e1f9ee9c2",
+    "url": "/static/profile.jpg"
+  },
+  {
+    "revision": "f866913f47c4b6b837a6920189f8b265",
+    "url": "/static/creditcards.png"
+  },
+  {
+    "revision": "2e88ba459d46a9001f05b408b3058809",
+    "url": "/static/salt-image-1.png"
+  },
+  {
+    "revision": "66524363461b77249e983bb30dd3894f",
+    "url": "/static/salt-image-3.png"
+  },
+  {
+    "revision": "cf3804871087b1f605cdb983702a0b02",
+    "url": "/static/salt-image-2.png"
+  },
+  {
+    "revision": "b920335ece6eef0cac097dabef93b4a1",
+    "url": "/static/salt-image-10.png"
+  },
+  {
+    "revision": "afb2bd6986a1e26cb9341e1feb6f0aa3",
+    "url": "/static/salt-image-4.png"
+  },
+  {
+    "revision": "c0833c0f6e8bd9be978827cc8754e4d8",
+    "url": "/static/salt-image-5.png"
+  },
+  {
+    "revision": "04c3a5f007cc5877607ad4412ab05342",
+    "url": "/static/salt-image-7.png"
+  },
+  {
+    "revision": "2868187fc12d3fa487dfcce279871e14",
+    "url": "/static/screeny1.png"
+  },
+  {
+    "revision": "98d4da0cf2a679cc36df597cd329a500",
+    "url": "/static/screeny2.png"
+  },
+  {
+    "revision": "12ba674e982cb545a152e2246d50eeb3",
+    "url": "/static/screeny3.png"
+  },
+  {
+    "revision": "b5c8bb366fedd517ef17750883c3061e",
+    "url": "/static/screeny4.png"
+  },
+  {
+    "revision": "07b1ca846f90464d4becfc345fe85f04",
+    "url": "/static/screeny5.png"
+  },
+  {
+    "revision": "b127f7589a1b597b853947114cce97f1",
+    "url": "/static/banner.jpg"
+  }
+];
