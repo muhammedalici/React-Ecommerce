29bd412338e72c3472c4c95e10601b2a8765f55b
muhammedalici <muhammedalici182@gmail.com>
2020-07-23 01:19:16 +0300

    Updates

---
+++ js/3.4caf588b516e7e8fe105.js
@@ -0,0 +1,28 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[3],{
+
+/***/ 393:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(0);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+ // Just add this feature if you want :P
+
+var UserOrdersTab = function UserOrdersTab() {
+  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "loader",
+    style: {
+      minHeight: '80vh'
+    }
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h3", null, "My Orders"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("strong", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("span", {
+    className: "text-subtle"
+  }, "You don't have any orders")));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (UserOrdersTab);
+
+/***/ })
+
+}]);
+//# sourceMappingURL=3.4caf588b516e7e8fe105.js.map
---
+++ js/4.cd26ae45032c20673d04.js
@@ -0,0 +1,28 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[4],{
+
+/***/ 392:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(0);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+ // Just add this feature if you want :P
+
+var UserWishListTab = function UserWishListTab() {
+  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "loader",
+    style: {
+      minHeight: '80vh'
+    }
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h3", null, "My Wish List"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("strong", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("span", {
+    className: "text-subtle"
+  }, "You don't have a wish list")));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (UserWishListTab);
+
+/***/ })
+
+}]);
+//# sourceMappingURL=4.cd26ae45032c20673d04.js.map
